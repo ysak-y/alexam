1d7bc9d070d39f60903dd31339707211cc1ae439
ysak-y <yoshiaki.0614@gmail.com>
2022-01-05 23:13:40 +0900

    Doesn't use toBeFalsy and toBeTruthly

--- __tests__/Alexam.spec.ts
+++ __tests__/Alexam.spec.ts
@@ -24,8 +24,8 @@ test("Update new property of session to false", async () => {
   const alexam = new AlexamBuilder()
     .setHandler(new LambdaHandler(handler))
     .build();
-  expect(alexam.requestFactory.session.new).toBeTruthy;
+  expect(alexam.requestFactory.session.new).toBe(true);
   const launchRequest = alexam.requestFactory.launchRequest();
   await alexam.send(launchRequest);
-  expect(alexam.requestFactory.session.new).toBeFalsy;
+  expect(alexam.requestFactory.session.new).toBe(false);
 });
