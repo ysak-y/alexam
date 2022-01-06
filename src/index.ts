b0e5d32a02e323f18153da3b184a570968040195
ysak-y <yoshiaki.0614@gmail.com>
2022-01-07 01:23:05 +0900

    Expose Session and Context to user import and customize it

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { Alexam } from "./Alexam";
 export { AlexamBuilder } from "./AlexamBuilder";
 export { SkillRequestFactory } from "./SkillRequestFactory";
 export { LambdaHandler, BaseHandler } from "./Handler";
+export { Session } from "./skillRequest/Session";
+export { Context } from "./skillRequest/Context";
